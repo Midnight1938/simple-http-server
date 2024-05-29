835c8d99d2fcbf0fbcff20a2f587b1aeacaf1084
skshm <eminate894567@protonmail.com>
2024-05-29 17:59:53 +0530

    URL Path handler

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,33 @@
 use http_server_starter_rust::status::HttpStatus;
 use std::{
-    io::{self, Read, Write},
-    net::TcpListener,
+    //    fmt::{self, Display, Formatter},
+    io::{self, BufRead, BufReader, Read, Write},
+    net::{self, TcpListener, TcpStream},
 };
+
+fn connection_handler(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let get = b"GET / HTTP/1.1\r\n";
+    let response = if buffer.starts_with(get) {
+        format!("{}\r\n", HttpStatus::Ok.into_status_line())
+    } else {
+        format!("{}\r\n", HttpStatus::NotFound.into_status_line())
+    };
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
 
 fn main() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
+            Ok(_stream) => {
                 println!("accepted new connection");
-                stream.write_fmt(format_args!("{}\r\n", HttpStatus::Ok.into_status_line()))?;
-                stream.flush()?;
+                connection_handler(_stream);
             }
             Err(e) => {
                 println!("error: {}", e);
