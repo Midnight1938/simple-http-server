9b3d18e19e58ffd200d3cb039020a80fbda353b9
skshm <eminate894567@protonmail.com>
2024-06-01 23:22:09 +0530

    stuff and body response

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,44 @@ use std::{
     net::{self, TcpListener, TcpStream},
 };
 
-fn connection_handler(mut stream: TcpStream) {
+fn connection_handler(mut stream: TcpStream) -> io::Result<()>{
     let mut buffer = [0; 512];
-    stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    stream.read(&mut buffer)?;
 
-    let get = b"GET / HTTP/1.1\r\n";
-    let response = if buffer.starts_with(get) {
-        format!("{}\r\n", HttpStatus::Ok.into_status_line())
-    } else {
-        format!("{}\r\n", HttpStatus::NotFound.into_status_line())
+    let request = String::from_utf8_lossy(&buffer[..]);
+    println!("Request: {}", request);
+    let lines: Vec<&str> = request.split("\r\n").collect();
+    let tokens: Vec<&str> = lines[0].split(" ").collect();
+
+
+    let response = match tokens.get(0) {
+        Some(&"GET") => {
+            if let Some(path) = tokens.get(1){
+                match *path {
+                    "/" | "/index.html" => format!("{}\r\n", HttpStatus::Ok.into_status_line()),
+                    content if content.starts_with("/echo") => {
+                        let tkn = content.replacen("/echo/", "", 1);
+                        format!("{}\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                                HttpStatus::Ok.into_status_line(), tkn.len(), tkn)
+                    }
+                    _ => format!("{}\r\n", HttpStatus::NotFound.into_status_line())
+                }
+            } 
+            else { format!("{}\r\n", HttpStatus::NotFound.into_status_line()) }
+        }
+        Some(_) => {
+            println!("Unknown method: {}", tokens[0]);
+            format!("{}\r\n", HttpStatus::MethodNotAllowed.into_status_line())
+        }
+        None => {
+            println!("No method specified");
+            format!("{}\r\n", HttpStatus::BadRequest.into_status_line())
+        }
     };
 
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    stream.write(response.as_bytes())?;
+    stream.flush()?;
+    Ok(())
 }
 
 fn main() -> Result<(), std::io::Error> {
@@ -26,11 +50,13 @@ fn main() -> Result<(), std::io::Error> {
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
-                println!("accepted new connection");
-                connection_handler(_stream);
+                eprintln!("accepted new connection");
+                if let Err(e) = connection_handler(_stream){
+                    eprintln!("Error Handling Connection: {}", e)
+                };
             }
             Err(e) => {
-                println!("error: {}", e);
+                eprintln!("Error accepting connection: {}", e);
             }
         };
     }
